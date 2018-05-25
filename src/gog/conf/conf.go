9b0dc37dd5b1f8ae8b29cc37c52674fac7448219
yueshizhixin <yueshizhixin@163.com>
2018-05-25 12:52:27 +0800

    Initial commit

---
+++ src/gog/conf/conf.go
@@ -0,0 +1,10 @@
+package conf
+
+var (
+	FILE_PATH string /*The file path of go-model file written*/
+)
+
+func init()  {
+	FILE_PATH="d:/go-model"
+}
+
---
+++ src/gog/file/file.go
@@ -0,0 +1,27 @@
+package file
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"log"
+	"gog/conf"
+)
+
+func writeFile(path string, data []byte) bool {
+	path = conf.FILE_PATH + "/" + path + ".go"
+	fmt.Print("reWrite'", path)
+	if _, err := ioutil.ReadDir(path); err != nil {
+		if err = os.Mkdir(conf.FILE_PATH, os.ModePerm); err == nil {
+			log.Fatal(err)
+			return false
+		}
+	}
+
+	if ioutil.WriteFile(path, data, 0644) == nil {
+		fmt.Println("'success")
+		return true
+	}
+	fmt.Println("'error")
+	return false
+}
